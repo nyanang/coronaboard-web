ad4f4fe1095a0ef0b8052da1259d699ed3473f1a
Bri <songhee1030@gmail.com>
2022-07-14 00:22:29 +0900

    09 대시보드와 공지사항 만들기

--- src/data-loader.js
+++ src/data-loader.js
@@ -2,31 +2,46 @@
 const axios = require ('axios');
 const {subDays} = require('date-fns');
 const {format, utcToZonedTime} = require ('date-fns-tz');
-
 const _ = require('lodash');
+
 const countryInfo = require('../../coronaboard/tools/downloaded/countryInfo.json');
+const ApiClient = require('./api-client');
+
+// 구글 시트로부터 내려받은 공지사항 목록 로드
+const notice = require('../../coronaboard/tools/downloaded/notice.json');
+
 
 async function getDataSource(){
     const countryByCc = _.keyBy(countryInfo, 'cc');
-    const globalStats = await generateGlobalStats();
+    const apiClient = new ApiClient();
+
+    // 국가별 데이터 로드
+    const allGlobalStats = await apiClient.getAllGlobalStats();
+
+    // 날짜별로 데이터를 묶는 부분을 기존 generateGlobalStats()함수에서 추출
+    const groupedByDate = _.groupBy(allGlobalStats, 'date');
+    const globalStats = generateGlobalStats(groupedByDate);
 
     return{
+        lastUpdated: Date.now(), // 데이터를 만든 현재 시간 기록
         globalStats,
         countryByCc,
+        //공지사항 몽록 중 hidden 필드가 false인 항목만 필터하여 전달
+        notice: notice.filter((x) => !x.hidden),
     };
 }
 
-async function generateGlobalStats(){
+function generateGlobalStats(groupedByDate){
     // HTTP 클라이언트 생성
-    const apiClient = axios.create({
-        baseURL: process.env.CORONABOARD_API_BASE_URL ||'http://localhost:8080',
-    });
+    // const apiClient = axios.create({
+    //     baseURL: process.env.CORONABOARD_API_BASE_URL ||'http://localhost:8080',
+    // });
 
-    // GET /global-stats API 호출
-    const response = await apiClient.get('global-stats');
+    // // GET /global-stats API 호출
+    // const response = await apiClient.get('global-stats');
 
-    // 날짜 기준 그룹핑
-    const groupedByDate = _.groupBy(response.data.result, 'date');
+    // // 날짜 기준 그룹핑
+    // const groupedByDate = _.groupBy(response.data.result, 'date');
 
     // 오늘/ 어제 날짜 생성
     // 데이터가 제공되는 마지막 날짜로 Date 객체 생성
--- src/templates/single-page.js
+++ src/templates/single-page.js
@@ -1,23 +1,57 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import {Slide} from '../components/slide';
+import { css }  from '@emotion/react';
+import { last } from 'lodash';
+import { Dashboard } from '../components/dashboard';
+import { Notice } from '../components/notice';
 
 export default function SinglePage({ pageContext}) {
+    // pageContext를 통해 전달된 데이터를 추출해서 사용
     const { dataSource} = pageContext;
     // 데이터 소스에서 원하는 필드 추출
-    const { countryByCc, globalStats} = dataSource;
+    const { lastUpdated, globalStats, notice} = dataSource;
+
+    // 사용자의 언어/지역 설정에 맞는 날짜 형태로 표시
+    const lastUpdatedFormatted = new Date(lastUpdated).toLocaleString();
+
     // 각 필드를 로그로 출력
-    console.log(countryByCc);
-    console.log(globalStats);
+    // console.log(countryByCc);
+    // console.log(globalStats);
     return (
-        <div>
-            <h1>
-                코로나보드
+        <div id ="top">
+            {/* 상단 검은색 배경 만들기 */}
+            <div
+                css={css`
+                    position: absolute;
+                    background-color: purple;
+                    width: 100%;
+                    height: 300px;
+                    z-index: -99;
+              `}
+            />
+            {/* 제목 표시 */}
+            <h1
+                css={css`
+                    padding-top: 48px;
+                    padding-bottom: 24px;
+                    color: white;
+                    text-align: center;
+                    font-size: 28px;
+                `}
+            >
+              *COVID-19*
+              <br />
+              REAL TIME INFO. 
             </h1>
-            {/* <p>
-                createPage로 만들어진 페이지입니다.
+            {/* 마지막 업데이트 정보 표시 */}
+            <p className="text-center text-white">
+                Last Updated: {lastUpdatedFormatted}
             </p>
-             */}
-            <Slide title = "국가별 현황"> 국가별 현황을 보여줍니다. </Slide>
+            <Dashboard globalStats={globalStats} />
+            <Notice notice={notice} />
+
+            {/* <Slide title = "국가별 현황"> 국가별 현황을 보여줍니다. </Slide> */}
             {/* <Slide title = {'대한민국 지역별 현황'}>
                 대한민국 지역별 현황을 보여줍니다.     
             </Slide> 
